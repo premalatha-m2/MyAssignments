44b4d470c9ed5c156135b32835869e19436332ee
premalatha-m2 <lathapremsa@gmail.com>
2025-06-22 18:27:06 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package Week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float Version = 91.0f;
+		String developer = "google" ;
+		boolean isbeta = false;
+		int releaseYear = 2000;
+		char shortcutkey = 'f'; 
+		System.out.println(Version +"\n" +developer+"\n"+isbeta+"\n"+releaseYear+"\n"+shortcutkey);
+		
+	}
+       
+}
